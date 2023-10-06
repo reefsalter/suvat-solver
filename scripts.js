4d5f2181d96ec26ac417417ad2822fe535a71446
Reef Salter <reefsalter@gmail.com>
2023-10-06 20:35:32 +0100

    fixed bugs

--- scripts.js
+++ scripts.js
@@ -1,42 +1,71 @@
 function solveSuvat() {
-    const s = parseFloat(document.getElementById('s').value);
-    const u = parseFloat(document.getElementById('u').value);
-    const v = parseFloat(document.getElementById('v').value);
-    const a = parseFloat(document.getElementById('a').value);
-    const t = parseFloat(document.getElementById('t').value);
+    let s = parseFloat(document.getElementById('s').value);
+    let u = parseFloat(document.getElementById('u').value);
+    let v = parseFloat(document.getElementById('v').value);
+    let a = parseFloat(document.getElementById('a').value);
+    let t = parseFloat(document.getElementById('t').value);
 
     let results = "";
-    let hasError = false;
+    let knownValuesCount = [s, u, v, a, t].filter(val => !isNaN(val)).length;
 
-    if (isNaN(s) || isNaN(u) || isNaN(v) || isNaN(a) || isNaN(t)) {
-        results += "<div class='error'>Error: Please enter valid numbers only!</div>";
-        hasError = true;
+    if (knownValuesCount !== 3) {
+        results += "<div class='error'>Error: Please provide exactly 3 known values!</div>";
+    } else if (document.getElementById('s').value.match(/[a-zA-Z]/g) || 
+               document.getElementById('u').value.match(/[a-zA-Z]/g) || 
+               document.getElementById('v').value.match(/[a-zA-Z]/g) || 
+               document.getElementById('a').value.match(/[a-zA-Z]/g) || 
+               document.getElementById('t').value.match(/[a-zA-Z]/g)) {
+        results += "<div class='error'>Error: Please enter valid numbers only. No characters allowed!</div>";
     } else {
-        if (!isNaN(u) && !isNaN(a) && !isNaN(t)) {
-            results += "<div class='equation'>s = ut + ½at²</div>";
-            results += "s = " + u + " × " + t + " + 0.5 × " + a + " × " + t + "²<br>";
-            results += "= " + (u * t + 0.5 * a * Math.pow(t, 2)).toFixed(2) + "<br><br>";
+        if (!isNaN(s) && !isNaN(u) && !isNaN(v)) {
+            t = (2 * s) / (u + v);
+            results += "<div class='equation'>t = 2s / (u + v) = " + t.toFixed(2) + " s</div>";
 
-            results += "<div class='equation'>v = u + at</div>";
-            results += "v = " + u + " + " + a + " × " + t + "<br>";
-            results += "= " + (u + a * t).toFixed(2) + "<br><br>";
+            a = (Math.pow(v, 2) - Math.pow(u, 2)) / (2 * s);
+            results += "<div class='equation'>a = (v² - u²) / 2s = " + a.toFixed(2) + " m/s²</div>";
+        } 
+        else if (!isNaN(s) && !isNaN(u) && !isNaN(a)) {
+            t = (-u + Math.sqrt(Math.pow(u, 2) + 2 * a * s)) / a;
+            results += "<div class='equation'>t = (-u + √(u² + 2as)) / a = " + t.toFixed(2) + " s</div>";
+
+            v = u + a * t;
+            results += "<div class='equation'>v = u + at = " + v.toFixed(2) + " m/s</div>";
+        } 
+        else if (!isNaN(s) && !isNaN(v) && !isNaN(a)) {
+            t = (v - Math.sqrt(Math.pow(v, 2) - 2 * a * s)) / a;
+            results += "<div class='equation'>t = (v - √(v² - 2as)) / a = " + t.toFixed(2) + " s</div>";
+
+            u = v - a * t;
+            results += "<div class='equation'>u = v - at = " + u.toFixed(2) + " m/s</div>";
+        } 
+        else if (!isNaN(u) && !isNaN(v) && !isNaN(t)) {
+            s = (u + v) * t / 2;
+            results += "<div class='equation'>s = (u + v)t / 2 = " + s.toFixed(2) + " m</div>";
+
+            a = (v - u) / t;
+            results += "<div class='equation'>a = (v - u) / t = " + a.toFixed(2) + " m/s²</div>";
+        } 
+        else if (!isNaN(u) && !isNaN(v) && !isNaN(a)) {
+            s = (Math.pow(v, 2) - Math.pow(u, 2)) / (2 * a);
+            results += "<div class='equation'>s = (v² - u²) / 2a = " + s.toFixed(2) + " m</div>";
+
+            t = (v - u) / a;
+            results += "<div class='equation'>t = (v - u) / a = " + t.toFixed(2) + " s</div>";
+        } 
+        else if (!isNaN(s) && !isNaN(u) && !isNaN(t)) {
+            v = (2 * s / t) - u;
+            results += "<div class='equation'>v = (2s / t) - u = " + v.toFixed(2) + " m/s</div>";
+
+            a = 2 * (s - u * t) / Math.pow(t, 2);
+            results += "<div class='equation'>a = 2(s - ut) / t² = " + a.toFixed(2) + " m/s²</div>";
+        } 
+        else if (!isNaN(s) && !isNaN(v) && !isNaN(t)) {
+            u = (2 * s / t) - v;
+            results += "<div class='equation'>u = (2s / t) - v = " + u.toFixed(2) + " m/s</div>";
+
+            a = 2 * (s - v * t) / Math.pow(t, 2);
+            results += "<div class='equation'>a = 2(s - vt) / t² = " + a.toFixed(2) + " m/s²</div>";
         }
-
-        if (!isNaN(u) && !isNaN(v) && !isNaN(a)) {
-            results += "<div class='equation'>s = (v² - u²) / 2a</div>";
-            results += "s = (" + v + "² - " + u + "²) / (2 × " + a + ")<br>";
-            results += "= " + ((Math.pow(v, 2) - Math.pow(u, 2)) / (2 * a)).toFixed(2) + "<br><br>";
-        }
-
-        if (!isNaN(u) && !isNaN(v) && !isNaN(t)) {
-            results += "<div class='equation'>s = ½(u + v)t</div>";
-            results += "s = 0.5 × (" + u + " + " + v + ") × " + t + "<br>";
-            results += "= " + (0.5 * (u + v) * t).toFixed(2) + "<br><br>";
-        }
-    }
-
-    if (!hasError && results === "") {
-        results += "<div class='error'>Insufficient data for any SUVAT equation!</div>";
     }
 
     document.getElementById('results').innerHTML = results;

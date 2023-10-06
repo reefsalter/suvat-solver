f57e4503e1270f5b0e24cbf6e8da3b61809a75e2
Reef Salter <reefsalter@gmail.com>
2023-10-06 20:58:51 +0100

    fix bugs

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+function validInput(val) {
+    return !isNaN(val) && val !== "";
+}
+
 function solveSuvat() {
     let s = parseFloat(document.getElementById('s').value);
     let u = parseFloat(document.getElementById('u').value);
@@ -10,63 +14,60 @@ function solveSuvat() {
 
     if (knownValuesCount !== 3) {
         results += "<div class='error'>Error: Please provide exactly 3 known values!</div>";
-    } else if (document.getElementById('s').value.match(/[a-zA-Z]/g) || 
-               document.getElementById('u').value.match(/[a-zA-Z]/g) || 
-               document.getElementById('v').value.match(/[a-zA-Z]/g) || 
-               document.getElementById('a').value.match(/[a-zA-Z]/g) || 
-               document.getElementById('t').value.match(/[a-zA-Z]/g)) {
-        results += "<div class='error'>Error: Please enter valid numbers only. No characters allowed!</div>";
     } else {
-        if (!isNaN(s) && !isNaN(u) && !isNaN(v)) {
-            t = (2 * s) / (u + v);
-            results += "<div class='equation'>t = 2s / (u + v) = " + t.toFixed(2) + " s</div>";
-
-            a = (Math.pow(v, 2) - Math.pow(u, 2)) / (2 * s);
-            results += "<div class='equation'>a = (v² - u²) / 2s = " + a.toFixed(2) + " m/s²</div>";
-        } 
-        else if (!isNaN(s) && !isNaN(u) && !isNaN(a)) {
-            t = (-u + Math.sqrt(Math.pow(u, 2) + 2 * a * s)) / a;
-            results += "<div class='equation'>t = (-u + √(u² + 2as)) / a = " + t.toFixed(2) + " s</div>";
-
+        if (validInput(u) && validInput(a) && validInput(t)) {
+            s = u * t + 0.5 * a * t * t;
             v = u + a * t;
-            results += "<div class='equation'>v = u + at = " + v.toFixed(2) + " m/s</div>";
-        } 
-        else if (!isNaN(s) && !isNaN(v) && !isNaN(a)) {
-            t = (v - Math.sqrt(Math.pow(v, 2) - 2 * a * s)) / a;
-            results += "<div class='equation'>t = (v - √(v² - 2as)) / a = " + t.toFixed(2) + " s</div>";
-
-            u = v - a * t;
-            results += "<div class='equation'>u = v - at = " + u.toFixed(2) + " m/s</div>";
-        } 
-        else if (!isNaN(u) && !isNaN(v) && !isNaN(t)) {
-            s = (u + v) * t / 2;
-            results += "<div class='equation'>s = (u + v)t / 2 = " + s.toFixed(2) + " m</div>";
-
+            results += `<div class='equation'>Using: s = ut + 0.5 * a * t² and v = u + at</div>
+                        Displacement, s = ${s.toFixed(2)} m<br>
+                        Final velocity, v = ${v.toFixed(2)} m/s<br>`;
+        } else if (validInput(u) && validInput(v) && validInput(t)) {
+            s = 0.5 * (u + v) * t;
             a = (v - u) / t;
-            results += "<div class='equation'>a = (v - u) / t = " + a.toFixed(2) + " m/s²</div>";
-        } 
-        else if (!isNaN(u) && !isNaN(v) && !isNaN(a)) {
-            s = (Math.pow(v, 2) - Math.pow(u, 2)) / (2 * a);
-            results += "<div class='equation'>s = (v² - u²) / 2a = " + s.toFixed(2) + " m</div>";
-
-            t = (v - u) / a;
-            results += "<div class='equation'>t = (v - u) / a = " + t.toFixed(2) + " s</div>";
-        } 
-        else if (!isNaN(s) && !isNaN(u) && !isNaN(t)) {
-            v = (2 * s / t) - u;
-            results += "<div class='equation'>v = (2s / t) - u = " + v.toFixed(2) + " m/s</div>";
-
-            a = 2 * (s - u * t) / Math.pow(t, 2);
-            results += "<div class='equation'>a = 2(s - ut) / t² = " + a.toFixed(2) + " m/s²</div>";
-        } 
-        else if (!isNaN(s) && !isNaN(v) && !isNaN(t)) {
+            results += `<div class='equation'>Using: s = 0.5 * (u + v) * t and a = (v - u) / t</div>
+                        Displacement, s = ${s.toFixed(2)} m<br>
+                        Acceleration, a = ${a.toFixed(2)} m/s²<br>`;
+        } else if (validInput(s) && validInput(v) && validInput(t)) {
             u = (2 * s / t) - v;
-            results += "<div class='equation'>u = (2s / t) - v = " + u.toFixed(2) + " m/s</div>";
-
-            a = 2 * (s - v * t) / Math.pow(t, 2);
-            results += "<div class='equation'>a = 2(s - vt) / t² = " + a.toFixed(2) + " m/s²</div>";
+            a = 2 * (v - u) / t;
+            results += `<div class='equation'>Using the rearrangements of s = 0.5 * (u + v) * t and a = (v - u) / t</div>
+                        Initial velocity, u = ${u.toFixed(2)} m/s<br>
+                        Acceleration, a = ${a.toFixed(2)} m/s²<br>`;
+        } else if (validInput(s) && validInput(u) && validInput(v)) {
+            t = 2 * s / (u + v);
+            a = (v * v - u * u) / (2 * s);
+            results += `<div class='equation'>Using rearrangements of s = 0.5 * (u + v) * t and v² = u² + 2as</div>
+                        Time, t = ${t.toFixed(2)} s<br>
+                        Acceleration, a = ${a.toFixed(2)} m/s²<br>`;
+        } else if (validInput(s) && validInput(u) && validInput(t)) {
+            v = (2 * s / t) - u;
+            a = 2 * (s - u * t) / (t * t);
+            results += `<div class='equation'>Using rearrangements of v = u + at and s = ut + 0.5 * a * t²</div>
+                        Final velocity, v = ${v.toFixed(2)} m/s<br>
+                        Acceleration, a = ${a.toFixed(2)} m/s²<br>`;
+        } else if (validInput(s) && validInput(v) && validInput(a)) {
+            u = Math.sqrt(v * v - 2 * a * s);
+            t = (v - u) / a;
+            results += `<div class='equation'>Using rearrangements of v² = u² + 2as and v = u + at</div>
+                        Initial velocity, u = ${u.toFixed(2)} m/s<br>
+                        Time, t = ${t.toFixed(2)} s<br>`;
+        } else if (validInput(u) && validInput(v) && validInput(a)) {
+            s = (v * v - u * u) / (2 * a);
+            t = (v - u) / a;
+            results += `<div class='equation'>Using rearrangements of s = 0.5 * (u + v) * t and v = u + at</div>
+                        Displacement, s = ${s.toFixed(2)} m<br>
+                        Time, t = ${t.toFixed(2)} s<br>`;
         }
     }
 
     document.getElementById('results').innerHTML = results;
+}
+
+function exportToImage() {
+    html2canvas(document.querySelector("#results")).then(canvas => {
+        let link = document.createElement('a');
+        link.href = canvas.toDataURL();
+        link.download = 'suvat-results.png';
+        link.click();
+    });
 }
